00002e82b003de3d22166bfe63b0815dface2551
Harry Venables <venablesh@yahoo.co.uk>
2015-09-02 18:25:47 +0100

    Completed post challenge 1

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -2,6 +2,12 @@ require './lib/bike.rb'
 
 class DockingStation
   def release_bike
-  	bike = Bike.new
+  	fail 'No bikes available' unless @bike
+  	@bike
   end
+
+  def dock(bike)
+  	@bike = bike
+  end
+
 end
