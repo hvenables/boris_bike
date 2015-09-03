daee0c63b31f73c0940287fb5cf91ed76c0c5c07
Lucy Fang <lucyfang2@gmail.com>
2015-09-03 17:36:33 +0100

    Post Challenge 2 - Finished first user story

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -2,18 +2,34 @@ require './lib/bike.rb'
 
 class DockingStation
 
+DEFAULT_CAPACITY = 20
+
+  attr_reader :capacity
   def initialize
     @bikes = []
+    @capacity = DEFAULT_CAPACITY
   end
 
   def release_bike
-  	fail 'No bikes available' if @bikes.empty?
-  	@bikes.pop
+  	fail 'No bikes available' if empty?
+  	bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bikes.count >= 20
-    @bikes << bike
+    fail 'Docking station full' if full?
+    bikes << bike
+  end
+
+  private
+
+  attr_reader :bikes
+
+  def empty?
+    bikes.empty?
+  end
+
+  def full?
+    bikes.count >= @capacity
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -19,10 +19,15 @@ describe DockingStation do
 
   describe '#dock' do
     it 'raises an error when full' do
-      20.times{ subject.dock(Bike.new) }
+      subject.capacity.times { subject.dock(Bike.new)}
       expect { subject.dock(Bike.new) }.to raise_error 'Docking station full'
     end
+
+    it "has a default capacity" do
+      expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+    end
   end
+
 
 end
 
